fc68bfbd873da7636a3ce2d3126c724726971711
Zixuan Guo <i@zxguo.me>
2019-12-07 03:40:54 -0600

    add json

--- model/fullplayer.go
+++ model/fullplayer.go
@@ -1,24 +1,27 @@
 package model
 
 type Statistics struct {
-	Rating            float32
-	KillsPerRound     float32
-	MapsPlayed        int
-	DeathsPerRound    float32
-	Headshots         float32
-	RoundsContributed float32
+	Rating            float32 `json:"rating"`
+	KillsPerRound     float32 `json:"kills_per_round"`
+	MapsPlayed        int `json:"maps_played"`
+	DeathsPerRound    float32 `json:"deaths_per_round"`
+	Headshots         float32 `json:"headshots"`
+	RoundsContributed float32 `json:"rounds_contributed"`
 }
 
 type FullPlayer struct {
-	ID           int
-	Name         *string //optional
-	Ign          string
-	Image        *string //optional
-	Age          *int    //optional
-	Country      Country
-	Team         Team
-	Twitter      *string
-	Facebook     *string
-	Statistics   Statistics
-	Achievements []Achievement
+	ID           int `json:"id"`
+	Name         *string `json:"name"`
+	Ign          string `json:"ign"`
+	Image        *string `json:"image"`
+	Age          *int    `json:"age"`
+	Country      Country `json:"country"`
+	Team         Team `json:"team"`
+	Twitter      *string `json:"twitter"`
+	Twitch       *string `json:"twitch"`
+	Facebook     *string `json:"facebook"`
+	Statistics   Statistics `json:"statistics"`
+	Achievements []Achievement `json:"achievements"`
 }
+
+
