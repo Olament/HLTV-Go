d98e3bc5e51336f004616cf1920ed8843b00084b
Zixuan Guo <i@zxguo.me>
2019-12-07 16:52:42 -0600

    merge to universal stats

--- model/fullplayer.go
+++ model/fullplayer.go
@@ -1,25 +1,16 @@
 package model
 
-type Statistics struct {
-	Rating            float32 `json:"rating"`
-	KillsPerRound     float32 `json:"kills_per_round"`
-	MapsPlayed        int     `json:"maps_played"`
-	DeathsPerRound    float32 `json:"deaths_per_round"`
-	Headshots         float32 `json:"headshots"`
-	RoundsContributed float32 `json:"rounds_contributed"`
-}
-
 type FullPlayer struct {
-	ID           int           `json:"id"`
-	Name         *string       `json:"name"`
-	Ign          string        `json:"ign"`
-	Image        *string       `json:"image"`
-	Age          *int          `json:"age"`
-	Country      Country       `json:"country"`
-	Team         Team          `json:"team"`
-	Twitter      *string       `json:"twitter"`
-	Twitch       *string       `json:"twitch"`
-	Facebook     *string       `json:"facebook"`
-	Statistics   Statistics    `json:"statistics"`
-	Achievements []Achievement `json:"achievements"`
+	ID           int
+	Name         *string
+	Ign          string
+	Image        *string
+	Age          *int
+	Country      Country
+	Team         Team
+	Twitter      *string
+	Twitch       *string
+	Facebook     *string
+	Statistics   Statistics
+	Achievements []Achievement
 }
--- model/fullplayerstats.go
+++ model/fullplayerstats.go
@@ -1,1 +1,29 @@
 package model
+
+type Statistics struct {
+	Kills                   int
+	Headshots               float32
+	Death                   int
+	KDRatio                 float32
+	DamgePerRound           float32
+	GrenadeDamge            float32
+	MapsPlayed              int
+	RoundsPlayed            int
+	KillsPerRound           float32
+	AssistsPerRound         float32
+	DeathsPerRound          float32
+	SavedByTeammatePerRound float32
+	SavedTeammatesPerRound  float32
+	Rating                  float32
+	RoundsContributed       float32
+}
+
+type FullPlayerStats struct {
+	name       *string
+	ign        *string
+	image      *string
+	age        *string
+	country    *string
+	team       *Team
+	statistics Statistics
+}
