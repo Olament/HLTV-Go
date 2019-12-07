2620c52dfeebcbc4a9d37fc502094ecdc82185a4
Zixuan Guo <i@zxguo.me>
2019-12-07 03:40:26 -0600

    add utils func

--- utils/parser.go
+++ utils/parser.go
@@ -1,1 +1,12 @@
 package utils
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"strings"
+)
+
+func PopSlashSource(selection *goquery.Selection) string {
+	res, _ := selection.Attr("src")
+	split := strings.Split(res, "/")
+	return split[len(split) - 1]
+}
