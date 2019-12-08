fb92ef61963b775c4dc2170701952e0b58ca447c
Zixuan Guo <i@zxguo.me>
2019-12-07 19:12:58 -0600

    new helpe function GetQuery()

--- utils/getter.go
+++ utils/getter.go
@@ -1,1 +1,28 @@
 package utils
+
+import (
+	"net/http"
+)
+
+func GetQuery(url string) (resp *http.Response, err error) {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36")
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != 200 {
+		return nil, &HTTPError{
+			Code:        res.StatusCode,
+			Description: http.StatusText(res.StatusCode),
+		}
+	}
+
+	return res, nil
+}
