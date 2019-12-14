39681e77474bf652e5b50d46c6f24b9c183929cf
Zixuan Guo <i@zxguo.me>
2019-12-14 16:56:59 -0600

    change func parameters to new enum

--- player.go
+++ player.go
@@ -4,7 +4,6 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"github.com/google/go-querystring/query"
 	"github.com/tidwall/gjson"
-	"hltv/enum"
 	"hltv/model"
 	"hltv/utils"
 	"io/ioutil"
@@ -137,8 +136,8 @@ func (h *HLTV) GetPlayerByName(name string) (player *model.FullPlayer, err error
 type PlayerStatsQuery struct {
 	StartDate  string //YYYY-MM-DD
 	EndDate    string
-	MatchType  enum.MatchType
-	RankFilter enum.RankingFilter
+	MatchType  MatchType
+	RankFilter RankingFilter
 }
 
 func (h *HLTV) GetPlayerStats(id int, q PlayerStatsQuery) (playerStats *model.FullPlayerStats, err error) {
